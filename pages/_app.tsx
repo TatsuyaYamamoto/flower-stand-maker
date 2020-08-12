a9f3996e3aba84bab61f0743f9b13c58d5149d68
Tatsuya Yamamoto <t28.is.tatsuya@gmail.com>
2020-08-13 03:13:09 +0900

    ssr support for material ui

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,37 @@
-import '../styles/globals.css'
+import React, { FC, useEffect } from "react";
+import { AppProps } from "next/app";
+import Head from "next/head";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+import { ThemeProvider } from "@material-ui/core/styles";
+import CssBaseline from "@material-ui/core/CssBaseline";
 
-export default MyApp
+import theme from "../src/theme";
+
+const MyApp: FC<AppProps> = (props) => {
+  const { Component, pageProps } = props;
+
+  useEffect(() => {
+    // Remove the server-side injected CSS.
+    const jssStyles = document.querySelector("#jss-server-side");
+    jssStyles?.parentElement?.removeChild(jssStyles);
+  }, []);
+
+  return (
+    <>
+      <Head>
+        <title>My page</title>
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width"
+        />
+      </Head>
+      <ThemeProvider theme={theme}>
+        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  );
+};
+
+export default MyApp;
