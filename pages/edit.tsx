700aaafc2249b0257c89936011215ae3bca301a2
Tatsuya Yamamoto <t28.is.tatsuya@gmail.com>
2020-08-13 01:08:15 +0900

    show bottom navigation for editor

---
+++ pages/edit.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { NextPage } from "next";
+
+import EditorBottomNavigation from "../src/components/organisms/EditorBottomNavigation";
+
+const EditPage: NextPage = () => {
+  return (
+    <div>
+      editor
+      <EditorBottomNavigation />
+    </div>
+  );
+};
+
+export default EditPage;
---
+++ src/components/organisms/EditorBottomNavigation.tsx
@@ -0,0 +1,57 @@
+import React, { FC } from "react";
+import { BottomNavigation, BottomNavigationAction } from "@material-ui/core";
+import {
+  Layers as LayerIcon,
+  InsertPhoto as ImageIcon,
+  LocationOn as LocationOnIcon,
+  LocalFlorist as FlowerIcon,
+} from "@material-ui/icons";
+import { makeStyles } from "@material-ui/styles";
+
+const useStyles = makeStyles({
+  root: {
+    position: "fixed",
+    bottom: 0,
+    left: "50%",
+    transform: "translate(-50%, 0)",
+  },
+});
+
+export type EditorBottomNavigationValue = "image" | "flower" | "layer";
+
+export interface EditorBottomNavigationProps {
+  onClick?: (value: EditorBottomNavigationValue) => void;
+}
+
+const EditorBottomNavigation: FC<EditorBottomNavigationProps> = (props) => {
+  const classes = useStyles();
+  const { onClick } = props;
+
+  const onChange = (_: any, value: EditorBottomNavigationValue) => {
+    if (onClick) {
+      onClick(value);
+    }
+  };
+
+  return (
+    <BottomNavigation className={classes.root} onChange={onChange}>
+      <BottomNavigationAction
+        label="Favorites"
+        value="image"
+        icon={<ImageIcon />}
+      />
+      <BottomNavigationAction
+        label="Nearby"
+        value="flower"
+        icon={<FlowerIcon />}
+      />
+      <BottomNavigationAction
+        label="Folder"
+        value="layer"
+        icon={<LayerIcon />}
+      />
+    </BottomNavigation>
+  );
+};
+
+export default EditorBottomNavigation;
