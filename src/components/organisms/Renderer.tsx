8b90796ff53412e3980159ea248e85ac96b70aac
Tatsuya Yamamoto <t28.is.tatsuya@gmail.com>
2020-08-19 13:41:04 +0900

    feat: select and hold sprite to edit

--- src/components/organisms/Renderer.tsx
+++ src/components/organisms/Renderer.tsx
@@ -1,10 +1,15 @@
 /** @jsx jsx */
-import React, { FC, useState } from "react";
-import { Stage, Sprite, Graphics } from "@inlet/react-pixi";
+import React, { FC, useCallback, useEffect, useState } from "react";
+import { Stage, Sprite, Graphics, Container } from "@inlet/react-pixi";
+import * as PIXI from "pixi.js";
 import { InteractionEvent } from "pixi.js";
 import { jsx, css } from "@emotion/core";
 
 import { Hierarchy } from "@/components/hooks/useHierarchy";
+
+// https://github.com/bfanger/pixi-inspector/issues/35#issuecomment-517811412
+// @ts-ignore
+window.__PIXI_INSPECTOR_GLOBAL_HOOK__?.register({ PIXI: PIXI });
 
 interface RendererProps {
   hierarchy: Hierarchy;
@@ -19,31 +24,88 @@ const Renderer: FC<RendererProps> = (props) => {
   const stageWidth = 300;
   const stageHeight = 400;
 
-  const [selectedHierarchyId, setSelectedHierarchyId] = useState<string | null>(
-    null
-  );
+  const [handlingObjectIds, setHandlingObjectIds] = useState<{
+    selected: string | null;
+    held: string | null;
+  }>({
+    selected: null,
+    held: null,
+  });
 
-  const onDragStart = (objectId: string) => ({ data }: InteractionEvent) => {
-    setSelectedHierarchyId(objectId);
-  };
+  const handleWindowClick = useCallback(() => {
+    console.log("pointerdown window");
 
-  const onDragMove = (objectId: string) => ({ data }: InteractionEvent) => {
-    if (objectId === selectedHierarchyId) {
-      const { x, y } = data.global;
+    if (handlingObjectIds.selected) {
+      setHandlingObjectIds((prev) => ({
+        ...prev,
+        selected: null,
+      }));
+    }
+  }, [handlingObjectIds]);
 
-      onMove({
-        objectId,
-        pointer: {
-          x: x / stageWidth,
-          y: y / stageHeight,
-        },
-      });
+  const onTapContainer = (e: PIXI.InteractionEvent) => {
+    console.log("pointerdown root container");
+
+    // prevent to fire event of document and window
+    e.data.originalEvent.stopPropagation();
+
+    if (handlingObjectIds.selected) {
+      setHandlingObjectIds((prev) => ({
+        ...prev,
+        selected: null,
+      }));
     }
   };
 
-  const onDragEnd = (objectId: string) => ({ data }: InteractionEvent) => {
-    setSelectedHierarchyId(null);
+  const onPointerDown = (objectId: string) => (e: InteractionEvent) => {
+    console.log("pointerdown", objectId, e.stopped);
+
+    // prevent to fire root container event
+    e.stopPropagation();
+    // prevent to fire window event
+    e.data.originalEvent.stopPropagation();
+
+    setHandlingObjectIds({
+      selected: objectId,
+      held: objectId,
+    });
   };
+
+  const onPointerMove = (e: InteractionEvent) => {
+    if (!e.target) {
+      // fired outside canvas
+      return;
+    }
+
+    if (!handlingObjectIds.held) {
+      // ignore because user don't hold any sprite.
+      return;
+    }
+
+    const { x, y } = e.data.global;
+    onMove({
+      objectId: handlingObjectIds.held,
+      pointer: {
+        x: x / stageWidth,
+        y: y / stageHeight,
+      },
+    });
+  };
+
+  const onPointerUp = (objectId: string) => ({ data }: InteractionEvent) => {
+    setHandlingObjectIds((prev) => ({
+      ...prev,
+      held: null,
+    }));
+  };
+
+  useEffect(() => {
+    window.addEventListener("pointerdown", handleWindowClick);
+
+    return () => {
+      window.removeEventListener("pointerdown", handleWindowClick);
+    };
+  }, []);
 
   return (
     <div
@@ -61,42 +123,48 @@ const Renderer: FC<RendererProps> = (props) => {
         height={stageHeight}
         options={{ transparent: true, autoDensity: true }}
       >
-        {hierarchy.map(({ objectId, url, pointer }) => {
-          const x = stageWidth * pointer.x;
-          const y = stageHeight * pointer.y;
-          const width = 100;
-          const height = 100;
+        {/* PIXI.app.stage に 同じevent, hitAreaを設定しても発火しないため、Stageと同じサイズのPIXI.Containerを独自で設定する */}
+        <Container
+          interactive={true}
+          pointerdown={onTapContainer}
+          hitArea={new PIXI.Rectangle(0, 0, stageWidth, stageHeight)}
+          pointermove={onPointerMove}
+        >
+          {hierarchy.map(({ objectId, url, pointer }) => {
+            const x = stageWidth * pointer.x;
+            const y = stageHeight * pointer.y;
+            const width = 100;
+            const height = 100;
 
-          return (
-            <React.Fragment key={objectId}>
-              {selectedHierarchyId === objectId && (
-                <Graphics
-                  draw={(g) => {
-                    g.clear();
-                    g.lineStyle(1, 0x111111, 0.5);
-                    // g.beginFill(0xff700b, 1);
-                    g.drawRect(x - width / 2, y - height / 2, width, height);
-                    g.endFill();
-                  }}
+            return (
+              <React.Fragment key={objectId}>
+                {handlingObjectIds.selected === objectId && (
+                  <Graphics
+                    draw={(g) => {
+                      g.clear();
+                      g.lineStyle(1, 0x111111, 0.5);
+                      // g.beginFill(0xff700b, 1);
+                      g.drawRect(x - width / 2, y - height / 2, width, height);
+                      g.endFill();
+                    }}
+                  />
+                )}
+                <Sprite
+                  image={url}
+                  x={x}
+                  y={y}
+                  width={width}
+                  height={height}
+                  anchor={0.5}
+                  interactive={true}
+                  buttonMode={true}
+                  pointerdown={onPointerDown(objectId)}
+                  pointerup={onPointerUp(objectId)}
                 />
-              )}
-              <Sprite
-                image={url}
-                x={x}
-                y={y}
-                width={width}
-                height={height}
-                anchor={0.5}
-                interactive={true}
-                buttonMode={true}
-                pointerdown={onDragStart(objectId)}
-                pointermove={onDragMove(objectId)}
-                pointerup={onDragEnd(objectId)}
-                pointerupoutside={onDragEnd(objectId)}
-              />
-            </React.Fragment>
-          );
-        })}
+              </React.Fragment>
+            );
+          })}
+        </Container>
       </Stage>
     </div>
   );
